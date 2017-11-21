c0bba55925ff287c174a64dd15bb0f99ee652712
AlexSzlachcic <aleksander.szlachcic@gmail.com>
2017-11-21 20:03:02 +0100

    Dodalem nowe imię

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
+        System.out.print("Witaj Javo! Moje Nowe Imie to Alex");
         System.out.print("Witaj Javo!");
     }
 
